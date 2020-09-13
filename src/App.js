5ea06c04ecf328e3076370bba822da96c01ce492
zhaoshuqi <18518664759@163.com>
2020-09-13 20:09:33 +0800

    初始化路由项目

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-          22222222
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//           22222222
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react'
+import Routes from './router'
+
+class App extends Component {
+    render() {
+        return (
+            <div classes="container">
+                <Routes />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'mobx-react'
+import store from './store'
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+    <Provider {...store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+)
