1cc357433951336839293b3f4234a5fdab929b4e
zhaoshuqi <18518664759@163.com>
2020-09-13 19:04:55 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>欢迎来到菜鸟教程</h2>
-        </header>
-        <p className="App-intro">
-          你可以在 <code>src/App.js</code> 文件中修改。
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+          22222222
+    </div>
+  );
 }
 
 export default App;
